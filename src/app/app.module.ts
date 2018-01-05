4f8ce66bd4cdc6313f5f9dd27af9072e36f7f72d
Chetan Dhanvij <chetan.dhanvij@pfizer.com>
2018-01-05 21:01:16 +0530

    Main code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { TermsAndConditionsPage } from '../pages/terms-and-conditions/terms-and-
 import { ComponentsModule } from '../components/components.module'
 import { BarcodeProvider } from '../providers/barcode/barcode';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { ContentProvider } from '../providers/content/content';
+import { ContentProvider } from '../providers/content/content'; 
 
 
 @NgModule({
